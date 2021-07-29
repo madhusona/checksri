ea1d457af933780616c1a869fd41209968376059
Madusudhanan <madusudhananb@gmail.com>
2021-07-29 16:10:17 +0530

    Update TestHello.java

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -11,20 +11,10 @@ import java.io.*;
 
 public class TestHello {
 
-   @Test
-   public void testHelloWorld()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
-
-     // action
-     Hello.main(null);
-
-     // assertion
-     assertEquals("Hello world!\n", bos.toString());
-
-     // undo the binding in System
-     System.setOut(originalOut);
-   }
+   Hello t = new Hello();
+	@Test
+	public void testAdd() 
+	{
+		assertEquals(25, t.add(10, 15));
+	}
 }
